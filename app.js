e2966321a1b0dbf43b5a880cb14158ec570c043a
leetusik <leetusik@gmail.com>
2025-05-10 15:52:30 +0900

    click event with toggle

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 h1 = document.querySelector("div h1");
 
 function handleClick() {
-  if (h1.classList.contains("clicked")) {
-    h1.classList.remove("clicked");
-  } else {
-    h1.classList.add("clicked");
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleClick);
